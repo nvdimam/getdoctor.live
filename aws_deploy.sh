aab3c8734921c12688d7b2137b2cd409cb03bbd5
Dima Mirkin <dima.mirkin@gmail.com.com>
2021-04-22 08:08:21 +0300

    Add deploy automation script

---
+++ aws_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Starting to Deploy...'
+ssh ec2-user@ec2-3-126-146-213.eu-central-1.compute.amazonaws.com " sudo docker image prune -f 
+        cd getdoctor.live 
+        sudo docker-compose down
+        git fetch origin
+        git reset --hard origin/master  &&  echo 'Pulled changes...'
+        sudo docker-compose build && sudo docker-compose up -d
+        "
+echo 'Deployment completed successfully'
